dd2d915727c9eb28cc0b6cbfd182e38951b890d4
Ismael Berón <portafolioismaelberon@gmail.com>
2024-04-09 12:28:03 +0200

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <footer className="w-full h-24 flex sm:flex-col sm:items-center  flex-wrap md:flex-row md:w-full items-center md:justify-around mt-2 bg-purple-800">
-      <p className="text-white font-bold text-4xl mx-auto alfa-slab text-center align-center pt-4">Associació de gats.</p>
+      <p className="text-white font-bold text-4xl mx-auto alfa-slab text-center align-center pt-4">Associació de gats</p>
       <p className="text-black font-bold text-xl mx-auto lobster pl-1 pt-2">Amics dels Gats i d'altres Animals de Catalunya</p>
     </footer>
   );

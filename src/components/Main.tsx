648b1268b984bd180a3131ca9aacbf26366ea2c6
Ismael Berón <portafolioismaelberon@gmail.com>
2024-04-09 18:08:16 +0200

    canvis colors i tipografies

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -78,10 +78,10 @@ const Main = () => {
         </div>
         <p className="text-2xl text-center amatic-bold mt-4"><span className="text-purple-600 alfa-slab text-xl">FES-TE SOCI!!</span></p>
         <p className="text-2xl text-center amatic-bold mt-4">Escull la frequència i la quota que millor et vagi, envians un correu per gestionar-ho!</p>
-        <div className= "sm:w-full md:w-2/4 md:mx-auto text-xl text-center flex-wrap indie-flower border-8 border-blue-400 rounded-xl mt-4 mb-4">
+        <div className= "sm:w-full md:w-2/4 md:mx-auto text-3xl text-center flex-wrap lora rounded-xl mt-4 mb-4">
           <a href="mailto:gatscat999@gmail.com">gatscat999@gmail.com</a></div>
         <p className="sm:w-full md:w-2/4 md:mx-auto text-center border-2 text-3xl border-black rounded-md bg-purple-300 mt-8 mb-3 p-1 lobster">Recorda que les donacions desgraven a la declaració d'Hisenda</p>
-        <p className="text-purple-600 alfa-slab text-6xl text-center mt-10 mb-10">XARXES</p>
+        <p className="text-amber-400 alfa-slab text-6xl text-center mt-10 mb-10">XARXES</p>
         <Xarxes />
       </section>
     </div>
--- src/components/Xarxes.tsx
+++ src/components/Xarxes.tsx
@@ -5,13 +5,13 @@ const Xarxes: React.FC = () => {
     return (
         <div className="flex gap-20 justify-center mb-10">
             <a href="https://instagram.com">
-                <FaInstagram size={80} color='#9333ea'/>
+                <FaInstagram size={80} color='#000'/>
             </a>
             <a href="https://www.facebook.com">
-                <FaFacebook color='#9333ea' size={80} />
+                <FaFacebook color='#000' size={80} />
             </a>
             <a href="mailto:gatscat999@gmail.com">
-                <FaEnvelope size={80} color='#9333ea' />
+                <FaEnvelope size={80} color='#000' />
             </a>
         </div>
     );
